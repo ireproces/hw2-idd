f4229bc1a5be4691b0cb64efae8a49aa66caff0b
Irene Proietti Cesarini <ireneproietticesarini@gmail.com>
2025-11-27 15:48:23 +0100

    Update java classes

--- txt-indexer-searcher/src/main/java/com/idd/util/TxtSearcher.java
+++ txt-indexer-searcher/src/main/java/com/idd/util/TxtSearcher.java
@@ -2,9 +2,9 @@ package com.idd.util;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.standard.StandardTokenizerFactory;
 import org.apache.lucene.analysis.custom.CustomAnalyzer;
 import org.apache.lucene.analysis.core.LowerCaseFilterFactory;
-import org.apache.lucene.analysis.core.WhitespaceTokenizerFactory;
 import org.apache.lucene.analysis.miscellaneous.WordDelimiterGraphFilterFactory;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.queryparser.classic.QueryParser;
@@ -29,17 +29,16 @@ public class TxtSearcher {
     private IndexSearcher searcher;
     private QueryParser titleParser;
     private QueryParser abstractParser;
-    // private long totalDocs;
 
+    // Costruttore
     public TxtSearcher(String indexPath) throws IOException {
         Directory dir = FSDirectory.open(Paths.get(indexPath));
         IndexReader reader = DirectoryReader.open(dir);
-        searcher = new IndexSearcher(reader);
-        // totalDocs = reader.numDocs();
+        this.searcher = new IndexSearcher(reader);
 
         // Analyzer personalizzato per il titolo
         Analyzer titleAnalyzer = CustomAnalyzer.builder()
-                .withTokenizer(WhitespaceTokenizerFactory.class)
+                .withTokenizer(StandardTokenizerFactory.class)
                 .addTokenFilter(LowerCaseFilterFactory.class)
                 .addTokenFilter(WordDelimiterGraphFilterFactory.class)
                 .build();
@@ -47,58 +46,81 @@ public class TxtSearcher {
         // Analyzer standard per l'abstract
         Analyzer abstractAnalyzer = new StandardAnalyzer();
 
-        titleParser = new QueryParser("title", titleAnalyzer);
-        abstractParser = new QueryParser("abstract", abstractAnalyzer);
+        this.titleParser = new QueryParser("title", titleAnalyzer);
+        this.abstractParser = new QueryParser("abstract", abstractAnalyzer);
     }
 
+    // Metodo che esegue la ricerca data una query in input
     public void search(String queryStr) throws Exception {
-        long startTime = System.currentTimeMillis(); // Inizia il timer
+
+        // Inizializzazione della varibile per le statistiche
+        long startTime = System.currentTimeMillis(); // Timer di inizio
 
         BooleanQuery.Builder booleanQueryBuilder = new BooleanQuery.Builder();
+        // Suddivide la query in termini separati dalla virgola e li salva in un array
         String[] terms = queryStr.split(",");
+        // Controlla che la query non contenga più di due termini
+        if (terms.length > 2) {
+            System.out.println("\nALERT: Invalid query format.\n");
+            return;
+        }
 
         boolean titleSearched = false, abstractSearched = false;
 
+        // Cicla sui termini per costruire la query effettiva
         for (String term : terms) {
+            // Suddivide il termine in campo da interrogare e termine di ricerca
             String[] parts = term.split(":", 2);
+            // Controlla che il termine sia nel formato corretto
             if (parts.length == 2) {
-                String field = parts[0].trim();
-                String fieldQuery = parts[1].trim();
+                String field = parts[0].trim(); // Campo da interrogare
+                String fieldQuery = parts[1].trim(); // Termine di ricerca
 
                 Query query = null;
+                // L'istruzione switch indirizza il controllo ad uno dei vari rami a seconda
+                // del valore della variabile 'field'
                 switch (field) {
+                    // Ramo di ricerca sul campo 'title'
                     case "title":
-                        query = titleParser.parse(fieldQuery);
+                        query = this.titleParser.parse(fieldQuery);
                         titleSearched = true;
                         break;
+                    // Ramo di ricerca sul campo 'abstract'
                     case "abstract":
-                        query = abstractParser.parse(fieldQuery);
+                        query = this.abstractParser.parse(fieldQuery);
                         abstractSearched = true;
                         break;
                     default:
-                        System.out.println("\nInvalid field. Valid fields are: title and abstract.\n");
+                        // Se il campo non è valido, stampa un messaggio di errore e termina la ricerca
+                        System.out.println("\nALERT: Invalid field. Valid fields are title and abstract.\n");
                         return;
                 }
 
                 booleanQueryBuilder.add(query, BooleanClause.Occur.MUST);
             } else {
-                System.out.println("\nInvalid query format. Use 'title:term', 'abstract:term', 'abstract:\"phrase\"' or 'title:x,abstract:y'.\n");
+                // Altrimenti, stampa un messaggio di errore e termina la ricerca
+                System.out.println("\nALERT: Invalid query format.\n");
                 return;
             }
         }
 
         BooleanQuery finalQuery = booleanQueryBuilder.build();
-        TopDocs results = searcher.search(finalQuery, 10);
-        long endTime = System.currentTimeMillis();
 
-        long totalDocumentsReturned = results.totalHits.value();
+        // Esegue una ricerca sincrona sull'indice Lucene e restituisce i primi
+        // 10 risultati con punteggio maggiore
+        TopDocs results = this.searcher.search(finalQuery, 10);
 
+        long endTime = System.currentTimeMillis(); // Timer di fine
+        long totalDocumentsReturned = results.totalHits.value(); // Numero totale di documenti restituiti
+
+        // Controlla che la ricerca abbia prodotto dei risultati
         if (results.totalHits.value() > 0) {
-            System.out.println("\n-----Documents-----");
-            System.out.println("Found " + results.totalHits.value() + " documents.\n");
+            System.out.println("\n-----" + results.totalHits.value() + " Documents retrieved-----");
 
+            // Cicla sui risultati ottenuti
             for (ScoreDoc hit : results.scoreDocs) {
-                Document doc = searcher.getIndexReader().storedFields().document(hit.doc);
+                // Recupera il documento corrispondente al risultato
+                Document doc = this.searcher.getIndexReader().storedFields().document(hit.doc);
 
                 // Stampa solo i campi richiesti
                 System.out.println("Title: " + (doc.get("title") != null ? doc.get("title") : "N/A"));
@@ -107,6 +129,7 @@ public class TxtSearcher {
                 System.out.println("\n");
             }
         } else {
+            // Altrimenti, stampa un messaggio di avviso
             System.out.println("\nNo documents found for the query.\n");
         }
 
@@ -118,7 +141,6 @@ public class TxtSearcher {
         // Statistiche di ricerca
         System.out.println("\n-----Search Statistics-----");
         System.out.printf("• Total documents returned: %d\n", totalDocumentsReturned);
-        // System.out.printf("• Total documents in the index: %d\n", totalDocs);
 
         double totalSeconds = (endTime - startTime) / 1000.0;
         System.out.printf("• Total time: %.2f seconds\n", totalSeconds);
@@ -126,32 +148,36 @@ public class TxtSearcher {
     }
 
     public void close() throws IOException {
-        searcher.getIndexReader().close();
+        this.searcher.getIndexReader().close();
     }
 
     public static void main(String[] args) {
-        try {
-            String indexPath = "indexes_lucene";
+
+        // Lettura dell'input da console
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String indexPath = "indexes_lucene"; // directory degli indici Lucene
             TxtSearcher searcher = new TxtSearcher(indexPath);
 
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("\nStarting...");
+            System.out.println("\nStarting the research program...");
             System.out.println("Enter your query ('title:term', 'abstract:term', 'abstract:\"phrase\"', 'title:x,abstract:y').");
             System.out.println("Or type 'exit' to stop the search.\n");
 
             String line;
+            // Ciclo infinito di lettura delle query
             while (true) {
                 System.out.print("Query > ");
 
-                line = br.readLine();
+                line = br.readLine(); // Legge la riga di input
+                // Controlla se la riga è valida o se l'utente ha digitato la keyword di uscita
                 if (line == null || line.equalsIgnoreCase("exit")) {
                     System.out.println("Exiting...\n");
                     break;
                 }
 
+                // Se la riga è valida, esegue la ricerca
                 if (!line.trim().isEmpty()) searcher.search(line);
             }
-
+            searcher.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
